89f0156ab98399ea42349984c8bc8c74a05e15de
parth <parth.j@coditude.com>
2025-12-09 21:38:05 +0530

    add cards section 1

--- src/Componenets/MainHome/MainHome.jsx
+++ src/Componenets/MainHome/MainHome.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSlider from './HeroSlider/HeroSlider'
+import CardsSection from './Cards/CardsSection'
 
 const MainHome = () => {
   return (
     <div>
       <HeroSlider/>
+      <CardsSection/>
     </div>
   )
 }
