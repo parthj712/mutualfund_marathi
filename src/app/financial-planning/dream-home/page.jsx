fff15c756a7a46a152523c7245a3d9e3536cc184
parth <parth.j@coditude.com>
2025-12-26 22:36:28 +0530

    add dream home calculator

---
+++ src/app/financial-planning/dream-home/page.jsx
@@ -0,0 +1,12 @@
+import DreamHome from '@/Componenets/MainFinancialPlan/AllCalculators/DreamHome/DreamHome'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <DreamHome />
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/financial-planning/wealth-creation/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>wealth-creation calcutor</div>
+  )
+}
+
+export default page
